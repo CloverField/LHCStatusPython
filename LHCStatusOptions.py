b61e08ca1aa8471ce505bb9f3a9621226d88c06d
Clover <blazelion3@gmail.com>
2018-12-24 20:10:22 -0500

    Defined Enums

---
+++ LHCStatusOptions.py
@@ -0,0 +1,15 @@
+from enum import Enum
+class StatusOptions(Enum):
+    CryoStatus = "Cryo Status"
+    CryoStatusIndividual = "Individual Cryo Status"
+    PCPermitStatus = "60 Amp PC Permit Status"  
+    PCPermitStatusIndividual = "Individual 60 Amp PC Permit Status" 
+    RFStatus = "RF Status"
+    RFStatusIndividual = "RF Status Individual" 
+    BeamDumpStatus = "Beam Dump Status"
+    BeamDumpComponentStatus = "Beam Dump Component Status"
+    EXPMagnetStatus = "Experiment Magnet Status"
+    EXPMagnetIndivdualStatus = "Individual Experiment Magnet Status"
+    Page1BeamStatus = "Page 1 Beam SMP Status"
+    Page1BeamSMPStatus = "Page 1 Individual SMP Beam Status"
+    PerformOCROnVistarPage = "Performs OCR on LHC Vistar page"
