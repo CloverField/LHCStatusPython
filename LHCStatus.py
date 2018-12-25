591ebb1e589b497adbe97c452c498c401e9f8f7d
Clover <blazelion3@gmail.com>
2018-12-24 20:36:39 -0500

    You can now select menu options.

--- LHCStatus.py
+++ LHCStatus.py
@@ -1,5 +1,14 @@
+import LHCStatusOptions
+
 def main():
-    print("Hello World!")
+    print("What do you want to check?")
+    i = 0
+    
+    for Option in (LHCStatusOptions.StatusOptions):
+        i += 1
+        print('{}. {}'.format(i,Option.value))
+
+    selection = input()   
 
 if __name__ == "__main__":
     main()
