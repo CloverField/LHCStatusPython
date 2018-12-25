027c3a13b026349fd54cb1b178264b5c5043bb50
Clover <blazelion3@gmail.com>
2018-12-25 12:22:02 -0500

    No longer doing that enum stuff.

--- LHCStatus.py
+++ LHCStatus.py
@@ -2,15 +2,30 @@ import LHCStatusOptions
 
 def main():
     print("What do you want to check?")
-    i = 0
-    
-    options = list(LHCStatusOptions.StatusOptions)
-
-    for Option in (LHCStatusOptions.StatusOptions):
-        i += 1
-        print('{}. {}'.format(i,Option.value))
+    print("1. Cryo Status")
+    print("2. Individual Cryo Status")
+    print("3. 60 Amp PC Permit Status")  
+    print("4. Individual 60 Amp PC Permit Status")
+    print("5. RF Status")
+    print("6. RF Status Individual")
+    print("7. Beam Dump Status")
+    print("8. Beam Dump Component Status")
+    print("9. Experiment Magnet Status")
+    print("10. Individual Experiment Magnet Status")
+    print("11. Page 1 Beam SMP Status")
+    print("12. Page 1 Individual SMP Beam Status")
+    print("13. Performs OCR on LHC Vistar page")
 
     selection = input()
+
+    if selection.isdigit():
+        if int(selection) == 1:
+            print("Selected Check Cryo")
+        elif int(selection) == 2:
+            pass
+        else:
+            pass
+
 
 if __name__ == "__main__":
     main()
