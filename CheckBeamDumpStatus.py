e23b2fff5e0f8ed867316e9e58f69631838153ad
Clover <blazelion3@gmail.com>
2019-01-04 18:30:51 -0500

    Implemented CheckBeamDumpIndividual.

--- CheckBeamDumpStatus.py
+++ CheckBeamDumpStatus.py
@@ -79,3 +79,103 @@ def GetBeamDumpStatus(Beam):
                 print("Looks like there is a error with the Beam 2 Beam Dump.")
                 return
         print("Everything looks good for the Beam 2 Beam Dump.")
+
+def CheckBeamDumpIndividual():
+    print("Which beam dump do you want to check?")
+    i = 0
+    for Option in (LHCStatusOptions.Beams):
+        i += 1
+        print('{}. {}'.format(i,Option.value))
+
+    selection = input()
+
+    if selection.isdigit():
+        if int(selection) == 1:
+            print("Which component do you want to check?")
+            i = 0
+            for Option in (LHCStatusOptions.Components):
+                i += 1
+                print('{}. {}'.format(i,Option.name.replace('_U_',' - ').replace('_',' ')))
+
+            selection = input()
+            if selection.isdigit():
+                if int(selection) == 1:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam1, LHCStatusOptions.Components.BeamDumped)
+                elif int(selection) == 2:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam1, LHCStatusOptions.Components.Kicker)
+                elif int(selection) == 3:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam1, LHCStatusOptions.Components.BETS)
+                elif int(selection) == 4:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam1, LHCStatusOptions.Components.IPOC_U_Beam_Dump_Pane)
+                elif int(selection) == 5:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam1, LHCStatusOptions.Components.LASS)
+                elif int(selection) == 6:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam1, LHCStatusOptions.Components.RETRIGGER)
+                elif int(selection) == 7:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam1, LHCStatusOptions.Components.XPOC)
+                elif int(selection) == 8:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam1, LHCStatusOptions.Components.REMOTE_U_Beam_Dump_Pane)
+                elif int(selection) == 9:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam1, LHCStatusOptions.Components.ON_U_Beam_Dump_Pane)
+                elif int(selection) == 10:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam1, LHCStatusOptions.Components.REMOTE_U_Injection_Pane)
+                elif int(selection) == 11:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam1, LHCStatusOptions.Components.ON_U_Injection_Pane)
+                elif int(selection) == 12:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam1, LHCStatusOptions.Components.TIMING_ON)
+                elif int(selection) == 13:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam1, LHCStatusOptions.Components.CONDITIONING)
+                elif int(selection) == 14:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam1, LHCStatusOptions.Components.TIMEOUT)
+                elif int(selection) == 15:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam1, LHCStatusOptions.Components.IPOC_U_Injection_Pane)
+                elif int(selection) == 16:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam1, LHCStatusOptions.Components.IQC)
+
+        elif int(selection) == 2:
+            print("Which component do you want to check?")
+            i = 0
+            for Option in (LHCStatusOptions.Components):
+                i += 1
+                print('{}. {}'.format(i,Option.name.replace('_U_',' - ').replace('_',' ')))
+
+            selection = input()
+            if selection.isdigit():
+                if int(selection) == 1:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam2, LHCStatusOptions.Components.BeamDumped)
+                elif int(selection) == 2:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam2, LHCStatusOptions.Components.Kicker)
+                elif int(selection) == 3:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam2, LHCStatusOptions.Components.BETS)
+                elif int(selection) == 4:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam2, LHCStatusOptions.Components.IPOC_U_Beam_Dump_Pane)
+                elif int(selection) == 5:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam2, LHCStatusOptions.Components.LASS)
+                elif int(selection) == 6:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam2, LHCStatusOptions.Components.RETRIGGER)
+                elif int(selection) == 7:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam2, LHCStatusOptions.Components.XPOC)
+                elif int(selection) == 8:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam2, LHCStatusOptions.Components.REMOTE_U_Beam_Dump_Pane)
+                elif int(selection) == 9:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam2, LHCStatusOptions.Components.ON_U_Beam_Dump_Pane)
+                elif int(selection) == 10:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam2, LHCStatusOptions.Components.REMOTE_U_Injection_Pane)
+                elif int(selection) == 11:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam2, LHCStatusOptions.Components.ON_U_Injection_Pane)
+                elif int(selection) == 12:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam2, LHCStatusOptions.Components.TIMING_ON)
+                elif int(selection) == 13:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam2, LHCStatusOptions.Components.CONDITIONING)
+                elif int(selection) == 14:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam2, LHCStatusOptions.Components.TIMEOUT)
+                elif int(selection) == 15:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam2, LHCStatusOptions.Components.IPOC_U_Injection_Pane)
+                elif int(selection) == 16:
+                    GetBeamDumpStatusIndividual(LHCStatusOptions.Beams.Beam2, LHCStatusOptions.Components.IQC)
+
+    else:
+        pass
+        
+def GetBeamDumpStatusIndividual(beam, component):
+    pass
