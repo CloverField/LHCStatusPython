5b6d02ab8e287f1e46f5ccb036c39508ee8a4063
Clover <blazelion3@gmail.com>
2019-01-04 21:17:19 -0500

    Implemented CheckExpMagnetIndividual.

--- CheckExpMagnetStatus.py
+++ CheckExpMagnetStatus.py
@@ -1,5 +1,6 @@
 from Web import getImage
 from Color import Color
+import LHCStatusOptions
 
 def CheckExpMagnetStatus():
     GetExpMagnetStatus()
@@ -23,3 +24,31 @@ def GetExpMagnetStatus():
             print("Not all Experiment magnets are functioning correctly.")
             return
     print("All Experiment magnets are functioning correctly.")
+
+def CheckExpMagnetIndividual():
+    print("Which magnet do you want to check?")
+    i = 0
+    for Option in (LHCStatusOptions.EXPMagnets):
+        i += 1
+        print('{}. {}'.format(i,Option.name.replace('_', ' ')))
+
+    selection = input()
+
+    if selection.isdigit():
+        if int(selection) == 1:
+            GetExpMagnetStatusIndividual(LHCStatusOptions.EXPMagnets.ALICE_solenoid)
+        elif int(selection) == 2:
+            GetExpMagnetStatusIndividual(LHCStatusOptions.EXPMagnets.ALICE_dipole)
+        elif int(selection) == 3:
+            GetExpMagnetStatusIndividual(LHCStatusOptions.EXPMagnets.ATLAS_solenoid)
+        elif int(selection) == 4:
+            GetExpMagnetStatusIndividual(LHCStatusOptions.EXPMagnets.ATLAS_torid)
+        elif int(selection) == 5:
+            GetExpMagnetStatusIndividual(LHCStatusOptions.EXPMagnets.CMS_solenoid)
+        elif int(selection) == 6:
+            GetExpMagnetStatusIndividual(LHCStatusOptions.EXPMagnets.LHCb_dipole)
+        else:
+            pass
+
+def GetExpMagnetStatusIndividual(magnet):
+    pass
